0d07657ca69c6f836b83c974f4ce88597d234969
sonvu <sonvu884@gmail.com>
2023-06-18 11:05:43 +0700

    update Premium_Weather_page.dart UI and highlight button

--- lib/pages/premium_weather_page.dart
+++ lib/pages/premium_weather_page.dart
@@ -1,9 +1,5 @@
 import 'package:exe201/pages/home_page.dart';
 import 'package:exe201/pages/premium_sounds_page.dart';
-import 'package:exe201/widgets/chart.dart';
-import 'package:exe201/widgets/percentage_rectangle.dart';
-import 'package:exe201/widgets/show_box.dart';
-import 'package:exe201/widgets/sleep_quality_linechart.dart';
 import 'package:flutter/material.dart';
 import '../widgets/bottom_navigation_bar.dart';
 import '../widgets/light_button.dart';
@@ -111,10 +107,31 @@ class _PremiumWeatherPage extends State<PremiumWeatherPage> {
                         Padding(
                           padding:
                           const EdgeInsets.fromLTRB(4, 0, 0, 0),
-                          child: LightButton(
-                            text: 'WEATHER',
-                            width: 110,
-                            height: 35, onPressed: () {  },),
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                  Color.fromRGBO(
+                                      53, 70, 112, 1),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                      BorderRadius.circular(
+                                          20.0))
+                                // minimumSize: Size(250, 40),
+                              ),
+                              child:
+
+                              Text(
+                                "WEATHER",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17.5,
+                                    fontFamily:
+                                    'Itim-Regular'),
+                              )
+
+
+                          ),
                         ),
                         Padding(
                           padding:
@@ -126,50 +143,45 @@ class _PremiumWeatherPage extends State<PremiumWeatherPage> {
                         ),
                       ]
                   ),
-
-
                 Expanded (
                     child:  ListView.builder(
                       itemCount: _data.length,
                       itemBuilder: (context, index) {
                         Item item = _data[index];
-
                         return ExpansionPanelList(
 
                           expansionCallback: (int panelIndex, bool isExpanded) {
-
                             setState(() {
                               _data.forEach((item) {
-                                item.isExpanded = false; // Đóng tất cả các thẻ
+                                item.isExpanded = false;
                               });
                               item.isExpanded = !isExpanded;
-                              // Mở hoặc đóng thẻ hiện tại
                             });
                           },
                           children: [
                             ExpansionPanel(
                               headerBuilder: (BuildContext context, bool isExpanded) {
                                 return ListTile(
-                                  tileColor:
-                                  const Color.fromARGB(
+                                  tileColor:const Color.fromARGB(
                                       156, 30, 90, 137),
-
                                   title: Container(
                                     child: Text(
                                       item.headerValue,
                                       style: TextStyle(
                                         color: Colors.white,
+                                        fontFamily:
+                                        'Itim-Regular',
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold,
                                       ),
                                     ),
                                   ),
                                 );
                               },
-
                               body: Container(
-                                color: Color.fromARGB(186, 20, 44, 121),
                                 child: _buildContentForDay(item.headerValue), // Call a method to build content based on the day
+                              color: Colors.cyan,
                               ),
-
                               isExpanded: item.isExpanded,
                             ),
                           ],
@@ -177,7 +189,6 @@ class _PremiumWeatherPage extends State<PremiumWeatherPage> {
                       },
                     ),
                   ),
-
               ],
             ),
           ),
@@ -192,12 +203,10 @@ class _PremiumWeatherPage extends State<PremiumWeatherPage> {
 }
 class Item {
   Item({
-    required this.expandedValue,
     required this.headerValue,
     this.isExpanded = false,
   });
 
-  String expandedValue;
   String headerValue;
   bool isExpanded;
 }
@@ -205,10 +214,7 @@ List<Item> generateItems() {
   List<String> daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   return daysOfWeek.map((day) {
     return Item(
-
       headerValue: day,
-      expandedValue: 'This is $day',
-
     );
   }).toList();
 }
@@ -217,14 +223,24 @@ Widget _buildContentForDay(String day) {
   switch (day) {
     case 'Monday':
       return Container(
-
           color: Color.fromARGB(186, 20, 44, 121),
           child: Row(
             children: [
-              Image.asset(
-                'assets/images/nhieumay.png',
-                width: 120,
-                height: 120,
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/images/nhieumay.png',
+                    width: 110,
+                    height: 110,
+                  ),
+                  Text("Cloudy",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: 'Itim-Regular',
+                    ),
+                  ),
+                ],
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -233,11 +249,11 @@ Widget _buildContentForDay(String day) {
                     "Ho Chi Minh city",
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 20,
+                      fontSize: 25,
                       fontFamily: 'Itim-Regular',
                     ),
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(height: 5),
                   Text(
                     "34°C",
                     style: TextStyle(
@@ -249,12 +265,12 @@ Widget _buildContentForDay(String day) {
                 ],
               ),
               SizedBox(
-                width: 30,
+                width: 15,
               ),
               Column(
                 children: [
                   Text(
-                    '16 Jun',
+                    '19 Jun',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 20,
@@ -278,14 +294,25 @@ Widget _buildContentForDay(String day) {
         );
     case 'Tuesday':
       return Container(
-
         color: Color.fromARGB(186, 20, 44, 121),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/muaset.png',
-              width: 120,
-              height: 120,
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/muaset.png',
+                  width: 110,
+                  height: 110,
+                ),
+                Text("Rain with \n lightning",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Itim-Regular',
+                  ),
+                ),
+              ],
+
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -294,13 +321,13 @@ Widget _buildContentForDay(String day) {
                   "Ho Chi Minh city",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 25,
                     fontFamily: 'Itim-Regular',
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 5),
                 Text(
-                  "29°C",
+                  "34°C",
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 25,
@@ -310,12 +337,12 @@ Widget _buildContentForDay(String day) {
               ],
             ),
             SizedBox(
-              width: 30,
+              width: 15,
             ),
             Column(
               children: [
                 Text(
-                  '17 Jun',
+                  '20 Jun',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
@@ -343,10 +370,21 @@ Widget _buildContentForDay(String day) {
         color: Color.fromARGB(186, 20, 44, 121),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/nhieumay.png',
-              width: 120,
-              height: 120,
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/nhieumay.png',
+                  width: 110,
+                  height: 110,
+                ),
+                Text("Cloudy",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Itim-Regular',
+                  ),
+                ),
+              ],
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -355,11 +393,11 @@ Widget _buildContentForDay(String day) {
                   "Ho Chi Minh city",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 25,
                     fontFamily: 'Itim-Regular',
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 5),
                 Text(
                   "31°C",
                   style: TextStyle(
@@ -371,12 +409,12 @@ Widget _buildContentForDay(String day) {
               ],
             ),
             SizedBox(
-              width: 30,
+              width: 15,
             ),
             Column(
               children: [
                 Text(
-                  '18 Jun',
+                  '21 Jun',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
@@ -404,10 +442,21 @@ Widget _buildContentForDay(String day) {
         color: Color.fromARGB(186, 20, 44, 121),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/mua.png',
-              width: 120,
-              height: 120,
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/mua.png',
+                  width: 110,
+                  height: 110,
+                ),
+                Text("Rainny",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Itim-Regular',
+                  ),
+                ),
+              ],
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -416,11 +465,11 @@ Widget _buildContentForDay(String day) {
                   "Ho Chi Minh city",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 25,
                     fontFamily: 'Itim-Regular',
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 5),
                 Text(
                   "30°C",
                   style: TextStyle(
@@ -432,12 +481,12 @@ Widget _buildContentForDay(String day) {
               ],
             ),
             SizedBox(
-              width: 30,
+              width: 15,
             ),
             Column(
               children: [
                 Text(
-                  '19 Jun',
+                  '22 Jun',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
@@ -465,10 +514,21 @@ Widget _buildContentForDay(String day) {
         color: Color.fromARGB(186, 20, 44, 121),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/muaset.png',
-              width: 120,
-              height: 120,
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/muaset.png',
+                  width: 110,
+                  height: 110,
+                ),
+                Text("Rain with \n lightning",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Itim-Regular',
+                  ),
+                ),
+              ],
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -477,11 +537,11 @@ Widget _buildContentForDay(String day) {
                   "Ho Chi Minh city",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 25,
                     fontFamily: 'Itim-Regular',
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 5),
                 Text(
                   "33°C",
                   style: TextStyle(
@@ -493,12 +553,12 @@ Widget _buildContentForDay(String day) {
               ],
             ),
             SizedBox(
-              width: 30,
+              width: 15,
             ),
             Column(
               children: [
                 Text(
-                  '20 Jun',
+                  '23 Jun',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
@@ -526,10 +586,21 @@ Widget _buildContentForDay(String day) {
         color: Color.fromARGB(186, 20, 44, 121),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/mua.png',
-              width: 120,
-              height: 120,
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/mua.png',
+                  width: 110,
+                  height: 110,
+                ),
+                Text("Rainny",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Itim-Regular',
+                  ),
+                ),
+              ],
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -538,11 +609,11 @@ Widget _buildContentForDay(String day) {
                   "Ho Chi Minh city",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 25,
                     fontFamily: 'Itim-Regular',
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 5),
                 Text(
                   "29°C",
                   style: TextStyle(
@@ -554,12 +625,12 @@ Widget _buildContentForDay(String day) {
               ],
             ),
             SizedBox(
-              width: 30,
+              width: 15,
             ),
             Column(
               children: [
                 Text(
-                  '21 Jun',
+                  '24 Jun',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
@@ -587,10 +658,21 @@ Widget _buildContentForDay(String day) {
         color: Color.fromARGB(186, 20, 44, 121),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/nhieumay.png',
-              width: 120,
-              height: 120,
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/nhieumay.png',
+                  width: 110,
+                  height: 110,
+                ),
+                Text("Cloudy",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Itim-Regular',
+                  ),
+                ),
+              ],
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -599,11 +681,11 @@ Widget _buildContentForDay(String day) {
                   "Ho Chi Minh city",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 25,
                     fontFamily: 'Itim-Regular',
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 5),
                 Text(
                   "32°C",
                   style: TextStyle(
@@ -615,12 +697,12 @@ Widget _buildContentForDay(String day) {
               ],
             ),
             SizedBox(
-              width: 30,
+              width: 15,
             ),
             Column(
               children: [
                 Text(
-                  '22 Jun',
+                  '25 Jun',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
